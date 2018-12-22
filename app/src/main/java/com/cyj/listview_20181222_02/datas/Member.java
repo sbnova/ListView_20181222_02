b35513d0724c40dc15df245ba94e66b43fc505f3
test <test1>
2018-12-22 12:22:13 +0900

    멤버 클래스에 생성자 도입

--- app/src/main/java/com/cyj/listview_20181222_02/datas/Member.java
+++ app/src/main/java/com/cyj/listview_20181222_02/datas/Member.java
@@ -8,6 +8,13 @@ public class Member {
 //    차후에 이런식으로 활용 연습
     private String profileImageUrl;
 
+//    생성자를 이용해 이름 / 주소 / url 한꺼번에 입력
+    public Member(String name, String address, String profileImageUrl){
+        this.name = name;
+        this.address = address;
+        this.profileImageUrl = profileImageUrl;
+    }
+
 //    각 변수의 값을 조회 or 세팅 getter & setter
 
 
